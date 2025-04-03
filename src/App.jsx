8da93cc2257d1cb4cf40a4b6bc78b5c8c8f7c703
royusjd <juodeikisrojus2005@gmail.com>
2025-04-03 11:14:52 +0300

    Updated files again

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
 import TopBanner from "./components/TopBanner";
+import Hero from "./components/Hero";
 
 export default function App() {
-	return(<>
-		<TopBanner />
-		<Header />
-	</>);
+	return (
+		<>
+			<TopBanner />
+			<Header />
+			<Hero />
+		</>
+	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,43 @@
 import "./component-style/Header.css";
 import Logo from "../assets/img/Logo.svg";
+import Search from "../assets/img/Search.svg";
+import cart from "../assets/img/cart.svg";
 
 export default function Header() {
-	return <div className="header-bg">
-				<div className="wrapper">
-					<a href="#"><img src={Logo} alt="Logo" /></a>
+	return (
+		<div className="header-bg">
+			<div className="wrapper header">
+				<a href="#">
+					<img
+						src={Logo}
+						alt="Logo"
+					/>
+				</a>
+				<nav className="header-links">
+					<a href="#">Home</a>
+					<a href="#">Products</a>
+					<a href="#">About Us</a>
+					<a href="#">Contact Us</a>
+				</nav>
+				<div className="header-right">
+					<div className="header-search">
+						Search
+						<img
+							src={Search}
+							alt="Search Icon"
+						/>
+					</div>
+					<div className="shopcart">
+						<a href="#">
+							<img
+								src={cart}
+								alt="Cart"
+							/>
+							<span className="number">3</span>
+						</a>
+					</div>
 				</div>
-			</div>;
+			</div>
+		</div>
+	);
 }
